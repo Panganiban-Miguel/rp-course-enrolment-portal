d87381cc407d9ceb50a67b441f20670090ebd545
Miguel <24022674@myrp.edu.sg>
2026-01-17 21:05:39 +0800

    fixed link

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/rp-course-enrolment-portal">
       <App />
     </BrowserRouter>
   </React.StrictMode>
